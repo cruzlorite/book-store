6e950cbfd0aacc41bfdc38e26b14da1ed373a3f7
José María <josemariacruzlorite@gmail.com>
2020-11-29 22:47:21 +0100

    database mongoose models created

---
+++ models/bookinstance.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema(
+  {
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true }, //reference to the associated book
+    imprint: {type: String, required: true},
+    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
+    due_back: {type: Date, default: Date.now}
+  }
+);
+
+// Virtual for bookinstance's URL
+BookInstanceSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+//Export model
+export default mongoose.model('BookInstance', BookInstanceSchema);
---
+++ models/genre.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+var Schema = mongoose.Schema;
+
+var GenreSchema = new Schema(
+  {
+    name: {type: String, required: true, maxlength=100, minlength=3}
+  }
+);
+
+// Virtual for genre's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+  return '/genre/' + this.name;
+});
+
+//Export model
+export default mongoose.model('Genre', GenreSchema);
