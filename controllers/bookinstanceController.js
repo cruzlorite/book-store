b72dda260c389b94c1e89d203be503275b5ec275
José María <josemariacruzlorite@gmail.com>
2020-12-04 16:04:25 +0100

    bookinstance_detail view added

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,9 +1,11 @@
+import mongoose from 'mongoose'
+
 import BookInstance from '../models/bookinstance'
 
 // Display list of all BookInstances.
 export function bookinstance_list(req, res, next) {
     BookInstance.find()
-      .populate('book')
+      .populate('book') // invoke populate to get details of the associated book
       .exec(function (err, list_bookinstances) {
         if (err) { return next(err); }
         // Successful, so render
@@ -12,8 +14,22 @@ export function bookinstance_list(req, res, next) {
   };
 
 // Display detail page for a specific BookInstance.
-export function bookinstance_detail(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+// Display detail page for a specific BookInstance.
+exports.bookinstance_detail = function(req, res, next) {
+    var id = mongoose.Types.ObjectId(req.params.id); 
+
+    BookInstance.findById(id)
+    .populate('book')
+    .exec(function (err, bookinstance) {
+      if (err) { return next(err); }
+      if (bookinstance==null) { // No results.
+          var err = new Error('Book copy not found');
+          err.status = 404;
+          return next(err);
+        }
+      // Successful, so render.
+      res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title, bookinstance:  bookinstance});
+    })
 };
 
 // Display BookInstance create form on GET.
