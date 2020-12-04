dbe7d6605b776bc295fbc668d92c30554d9ac477
José María <josemariacruzlorite@gmail.com>
2020-12-04 15:48:30 +0100

    genre_detail view added

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,8 @@
+import async from 'async'
+import mongoose from 'mongoose'
+
 import Genre from '../models/genre'
+import Book from '../models/book'
 
 // Display list of all Genre.
 export function genre_list(req, res) {
@@ -12,8 +16,29 @@ export function genre_list(req, res) {
 };
 
 // Display detail page for a specific Genre.
-export function genre_detail(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+export function genre_detail(req, res, next) {
+    var id = mongoose.Types.ObjectId(req.params.id); 
+
+    async.parallel({
+        // fetch genre name by id 
+        genre: function(callback) {
+            Genre.findById(id).exec(callback);
+        },
+        // fetch all books of specified genre
+        genre_books: function(callback) {
+            Book.find({ 'genre': id }).exec(callback);
+        },
+    },
+    function(err, results) {
+        if (err) { return next(err); }
+        if (results.genre == null) { // No results.
+            var err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books } );
+    });
 };
 
 // Display Genre create form on GET.
